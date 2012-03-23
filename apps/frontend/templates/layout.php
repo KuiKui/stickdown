7d2b35c0d6730938b6318192a24ee2dedf6e07a1
Mickael Despesse <mickael.despesse@pmsipilot.com>
2012-03-23 11:15:04 +0100

    Ajout de GoogleAnalytics

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -9,6 +9,16 @@
     <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Armata' />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-30234218-1']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
   </head>
   <body>
   <div class="navbar">
